e73b1b75ae94a6a296c21625ef618491199fc96f
lind1125 <danieljlinden@gmail.com>
2020-09-13 20:45:14 -0500

    Callbacks, function definition placement, error reading

--- combining_datatypes.js
+++ combining_datatypes.js
@@ -65,3 +65,84 @@ const knit = () => {
   }
   
   powerButton()()
+
+  // **Model a Vending Machine**
+
+// - a vending machine is an object
+// - it has an array of snacks (make 3 snacks)
+// - snacks are objects that have a name and a price
+// - a vending machine has a function vend that allows user to enter the array position (a number) of the snack and then that snack will be returned
+// - Be able to call vendingMachine.vend() with a valid integer to return a snack
+
+const vendingMachine = {
+    snacks: [
+    {name: 'Cookie Cat', price: 2.50}, 
+    {name: 'Fried Cheezers', price: 1.25}, 
+    {name: 'Just Gum', price: 0.75}
+    ],
+   vend: function(num) {
+     return vendingMachine.snacks[num]
+   }
+  }
+  
+  vendingMachine.vend(0)
+
+  // ## Callbacks
+// 1. Make a function `add` that takes two arguments (numbers) and sums them together
+const add = (x, y) => {
+    return x+y
+}
+  // 1. Make a function `subtract` that takes two arguments (numbers) and subtracts them
+const subtract = (x, y) => {
+    return x-y
+}
+  // 1. Make a function `multiply` that takes two arguments and multiplies them
+const multiply = (x, y) => {
+    return x*y
+}
+  // 1. Make a function `divide` that takes two arguments and divides them
+const divide = (x, y) => {
+    return x/y
+}
+  // 1. Make a function `calculate` that takes three arguments. Assume the two arguments are a number  ie `num1, num2` and a function called `operates` (a callback).
+  
+  // 1. Make it so that when `calculate` is invoked, the callback "operates" on the numbers and returns the value.
+const calculate = (num1, num2, operates) => {
+    return operates(num1, num2)
+}
+    
+  // 1. Call `calculate` 4 times, each time using one of the operation functions you wrote
+calculate(12,3,add)
+calculate(12,3,subtract)
+calculate(12,3,multiply)
+calculate(12,3,divide)
+  // #### Function definition placement
+
+// Clean up this code, so that it works and has function definitions in the correct place
+
+const bar = () => {
+    console.log('bar here');
+}
+
+const foo = () => {
+    console.log('foo here');
+}
+
+bar();
+foo();
+// #### Error reading
+
+// What is meant by the error(s) this produces?
+
+foo();
+
+const foo ()=>{
+    console.log('hi');
+}
+// ###SyntaxError: Missing initializer in const declaration###
+// There is a = missing after foo and before the () to declare foo as a function
+
+// After fixing the SyntaxError...
+
+// ###ReferenceError: Cannot access 'foo' before initialization###
+// The foo function is being invoked before it has been defined
