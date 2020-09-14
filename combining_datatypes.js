bd816af83ca06c61d8545154b4f7e7c5f57ce0a6
lind1125 <danieljlinden@gmail.com>
2020-09-13 19:41:35 -0500

    Combine objects, arrays, and functions more than one level deep

--- combining_datatypes.js
+++ combining_datatypes.js
@@ -32,3 +32,36 @@ const crayonBox = {
    ]
    
    console.log(apartmentBuilding[1][1])
+
+   // ## Combine objects, arrays, and functions more than one level deep
+
+// 1. Create a function `knit` that returns an object that has the following kinds of properties `item`: `scarf`, `size` : `6ft` etc.  Log a value of that object (hint: call the function and then call a property on the return value).
+const knit = () => {
+    return {
+      item: 'scarf',
+      size: '6ft',
+      color: 'green',
+      danglyBits : true
+    }
+  }
+  
+  console.log(knit().size)
+  
+  // 1. Create a function `crayonSelector` that returns an object that has an array (you can reuse your `crayonBox` object).  Log one of the elements of that array.
+  const crayonSelector = () => {
+  return crayonBox = {
+      colors: ['forest green', 'sunshine yellow', 'ocean blue', 'non-controversial beige']
+     }
+  }
+  
+  console.log(crayonSelector().colors[1])
+  
+  // 1. Create a function `powerButton` that returns a function called `options` - `options` should console.log a simple message like `select a song`.  Call that inner function
+  
+  const powerButton = () => {
+    return options = () => {
+      console.log('select a song')
+    }
+  }
+  
+  powerButton()()
